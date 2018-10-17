1cfac61bad66121809eaf95cc38e2bf204adceae
Rod Ta <rody101@gmail.com>
2018-10-17 14:31:55 -0400

    about python

---
+++ python_syntax/ascii_char.py
@@ -0,0 +1,11 @@
+char = 'a'
+
+# Get the ASCII number of a character
+number = ord(char)
+
+# Get the character given by an ASCII number
+char = chr(number)
+
+print(number, char)
+
+''' 97, a '''
---
+++ python_syntax/join.py
@@ -0,0 +1,13 @@
+test =  {'2', '1', '3'}
+s = ', '
+print(s.join(test))
+
+test = {'Python', 'Java', 'Ruby'}
+s = '->->'
+print(s.join(test))
+
+'''
+returns:
+2, 3, 1
+Python->->Ruby->->Java
+'''
